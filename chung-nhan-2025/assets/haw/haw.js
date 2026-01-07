c76a9f6dd500cc81e5ea7d11bfb1c5e2ba1856cc
khuyennth <khuyen.nguyen@anphabe.com>
2026-01-07 17:03:09 +0700

    update

---
+++ chung-nhan-2025/assets/haw/haw.js
@@ -0,0 +1,283 @@
+(function ($) {
+    let apiURL = 'https://csbe.vietnambestplacestowork.com/api/export-company-award/{award}/{year}'
+
+    let template = {
+        "grid": {
+            "<>": "div", "class": "row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5", "html": function () {
+                return ($.json2html(this.companies, template.grid_items));
+            }
+        },
+
+        "grid_items": {
+                "<>": "div", "class": "col ranking-item p-0 m-0 border-0", "html": [
+                    {
+                        "<>": "div", "class": "card h-100 bg-white mbr-text border border-0", "html": [
+                            {
+                                "<>": "div", "class": "card-body", "html": [
+                                    {
+                                        "<>": "div",
+                                        "style": "line-height: 1em; font-size: 16px; font-weight: 500; position: absolute;top: 0;left: 0;background: orange;padding: 10px;border-radius: 0.25rem 0 0 0;color: white;",
+                                        "class": function () {
+                                            if (this.ranking == 0) {
+                                                return "d-none";
+                                            } else {
+                                                return "ranking-position";
+                                            }
+                                        },
+                                        "html": "#${ranking}"
+                                    },
+                                    {
+                                        "<>": "a",
+                                        "href": function () {
+                                            if (this.portal_url.length > 0) {
+                                                return this.portal_url;
+                                            } else {
+                                                return "#";
+                                            }
+                                        },
+                                        "html": [
+                                            {
+                                                "<>": "img",
+                                                "src": "${logo_link}",
+                                                "class": "mx-auto d-block pt-2 pb-4",
+                                                "alt": "${name}",
+                                                "style": "width: initial",
+                                                "html": ""
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "<>": "h5",
+                                        "class": "card-title fs-6 text-center",
+                                        "html": function () {
+                                            if (this.portal_url.length > 0) {
+                                                return '<a class="text-black" href="' + this.portal_url + '"' + '>' + this.name + '</a>'
+                                            } else {
+                                                return this.name;
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "<>": "p",
+                                        "class": "card-text text-dark display-7 d-none",
+                                        "html": function () {
+
+                                            if (this.description.length > 250) {
+                                                return this.description.substring(0, 250) + '...'
+                                            }
+                                            return this.description;
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+
+        },
+        "list": {
+            "<>": "div", "class": "row row-cols-1 row-cols-lg-2", "html": function () {
+                return ($.json2html(this.companies, template.list_items));
+            }
+        },
+
+        "list_items": {
+            
+                "<>": "div", "class": "col ranking-item ", "html": [
+                    {
+                        "<>": "div", "class": "d-flex mbr-text text-center align-items-center  border-bottom p-2", "html": [
+                            
+                                
+                            {
+                                "<>": "div",
+                                "style": "width:80px; padding:12px 0; background: #d2e8f3",
+                                "class": function () {
+                                    if (this.ranking == 0) {
+                                        return "d-none";
+                                    } else {
+                                        return "ranking-position rank-num fs-4 text-success flex-shrink-0";
+                                    }
+                                },
+                                "html": "#${ranking}"
+                            },
+                            {
+                                "<>": "a",
+                                "class": "px-4",
+                                "href": function () {
+                                    if (this.portal_url.length > 0) {
+                                        return this.portal_url;
+                                    } else {
+                                        return "#";
+                                    }
+                                },
+                                "html": [
+                                    {
+                                        "<>": "img",
+                                        "src": "${logo_link}",
+                                        "class": "mx-auto d-block",
+                                        "alt": "${name}",
+                                        "style": "width: initial",
+                                        "html": ""
+                                    }
+                                ]
+                            },
+                            {
+                                "<>": "h5",
+                                "class": "px-4 fs-6 text-center",
+                                "html": function () {
+                                    if (this.portal_url.length > 0) {
+                                        return '<a class="text-black" href="' + this.portal_url + '"' + '>' + this.name + '</a>'
+                                    } else {
+                                        return this.name;
+                                    }
+                                }
+                            },
+                        ]
+                    }
+                ]
+          
+        },
+        
+    };
+    
+    function rankingDisplayByJson2HTML(data) {
+        $('.loading-data').hide();
+
+        data.sort(function(a,b) {return a.sortname.localeCompare(b.sortname);});
+        $("#vnbptw-ranking").empty().json2html({'companies':data}, template.grid);
+
+
+        $("#ranking-filter").on("keyup", function () {
+            let value = remove_vn_character($(this).val().toLowerCase());
+            $("#vnbptw-ranking .ranking-item").filter(function () {
+                let search = remove_vn_character($(this).text().toLowerCase());
+                $(this).toggle(search.indexOf(value) > -1)
+            });
+        });
+        $('#bptw-view-grid').click(function () {
+            $("#ranking-filter").val("");
+            $("#vnbptw-ranking").empty().json2html({'companies':data}, template.grid);
+        })
+        $('#bptw-view-table').click(function () {
+            $("#ranking-filter").val("");
+            $("#vnbptw-ranking").empty().json2html({'companies':data}, template.list);
+        })
+    }
+    function getData(url) {
+        $.get(url, function (data, status) {
+            
+            rankingDisplayByJson2HTML(data);
+
+            if (window.location.pathname.includes('top10-haw')) {
+                $('#bptw-view-table').click();
+            }
+        });
+    }
+    function getDataURL() {
+        let award = $('#award').val();
+        let year = $('#select_year').val();
+        let url = new URL(window.location.href);
+            
+        let industry = $('#select_industry').val();
+        let dataURL = apiURL.replace('{year}', year);
+        if (industry) {
+            dataURL = dataURL.replace('{award}', industry)    
+        }
+        else {
+            dataURL = dataURL.replace('{award}', award);
+        }
+        console.log(dataURL);
+        return dataURL;
+    }
+
+    $(document).ready(function () {
+        if($("#award").length > 0 && $("#select_year").length > 0) {
+            getData(getDataURL());
+
+            $("#select_industry").change(function () {
+                getData(getDataURL());
+            });
+            
+            $('#select_year').change(function () {
+                let page = $(this).find(':selected').val();
+                let domain = window.location.protocol + '//' + window.location.hostname;
+                window.location.href = domain + '/' + page;
+            });
+        }
+    });
+    
+
+
+/**
+ * Adds options to the year select element based on the years
+ * defined in the `years` array. The selected option is set to
+ * the current page URL.
+ *
+ * @name addOptionsToYearSelect
+ * @function
+ * @return {undefined} This function does not return a value.
+ */
+    function addOptionsToYearSelect() {
+        var yearSelect = document.getElementById("select_year");
+        
+        var years = [
+            { year: 2024, url: "chung-nhan-2024/index.html" },
+            { year: 2023, url: "chung-nhan-2023.html" },
+            { year: 2022, url: "chung-nhan-2022.html" },
+            { year: 2021, url: "chung-nhan-2021.html" },
+        ];
+
+        for (var i = 0; i < years.length; i++) {
+            var option = document.createElement("option");
+            option.value = years[i].url;
+            option.text = years[i].year;
+            if (years[i].url == window.location.pathname) {
+                option.selected = true;
+            }
+            yearSelect.appendChild(option);
+        }
+    }
+
+    
+
+    function remove_vn_character(input) {
+        if (!input) {
+            return '';
+        }
+        var str = input.split("");
+        for (var i = 0; i < str.length - 1; i++) {
+            if (str[i] == 'á' || str[i] == 'à' || str[i] == 'ả' || str[i] == 'ã' || str[i] == 'ạ' || str[i] == 'ă' || str[i] == 'ắ' ||
+                str[i] == 'ặ' || str[i] == 'ằ' || str[i] == 'ẳ' || str[i] == 'ẵ' || str[i] == 'â' || str[i] == 'ấ' || str[i] == 'ầ' ||
+                str[i] == 'ẩ' || str[i] == 'ẫ' || str[i] == 'ậ') {
+                str[i] = 'a';
+            }
+            if (str[i] == 'é' || str[i] == 'è' || str[i] == 'ẻ' || str[i] == 'ẽ' || str[i] == 'ẹ' || str[i] == 'ê' || str[i] == 'ế' || str[i] == 'ề' ||
+                str[i] == 'ể' || str[i] == 'ễ' || str[i] == 'ệ') {
+                str[i] = 'e';
+            }
+            if (str[i] == 'đ') {
+                str[i] = 'd';
+            }
+            if (str[i] == 'í' || str[i] == 'ì' || str[i] == 'ỉ' || str[i] == 'ĩ' || str[i] == 'ị') {
+                str[i] = 'i';
+            }
+            if (str[i] == 'ó' || str[i] == 'ò' || str[i] == 'ỏ' || str[i] == 'õ' || str[i] == 'ọ' || str[i] == 'ô' || str[i] == 'ố' || str[i] == 'ồ' ||
+                str[i] == 'ổ' || str[i] == 'ỗ' || str[i] == 'ộ' || str[i] == 'ơ' || str[i] == 'ớ' || str[i] == 'ờ' || str[i] == 'ở' || str[i] == 'ỡ' ||
+                str[i] == 'ợ') {
+                str[i] = 'o';
+            }
+            if (str[i] == 'ú' || str[i] == 'ù' || str[i] == 'ủ' || str[i] == 'ũ' || str[i] == 'ụ' || str[i] == 'ư' || str[i] == 'ứ' ||
+                str[i] == 'ừ' || str[i] == 'ử' || str[i] == 'ữ' || str[i] == 'ự') {
+                str[i] = 'u';
+            }
+            if (str[i] == 'ý' || str[i] == 'ỳ' || str[i] == 'ỷ' || str[i] == 'ỹ' || str[i] == 'ỵ') {
+                str[i] = 'y';
+            }
+        }
+        ;
+        var text = str.join();
+
+        return text.replace(/,/g, "");
+    };
+
+})(jQuery);
